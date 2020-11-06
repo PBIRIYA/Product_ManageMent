431903a6299e0b1949a615c2260b0bd6c7e68f72
Priya Biriya <priyabiriya1707@gmail.com>
2020-11-07 01:51:32 +0530

    [Priya] add. welcome message

---
+++ Product_ManageMent/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Product_ManageMent
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to product management program!");
+        }
+    }
+}
