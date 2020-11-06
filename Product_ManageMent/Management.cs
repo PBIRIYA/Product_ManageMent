17ba2f36c089ebbc1b41b54553315e8dc3d35c53
Priya Biriya <priyabiriya1707@gmail.com>
2020-11-07 02:25:16 +0530

    [Priya] add. select records

--- Product_ManageMent/Management.cs
+++ Product_ManageMent/Management.cs
@@ -14,5 +14,16 @@ namespace Product_Management
                               orderby productReviews.Rating descending
                               select productReviews).Take(3);
         }
+        public void SelectRecords(List<ProductReview> ListProductReview)
+        {
+            var SelectRecords = from ProductReviews in ListProductReview
+                                where (ProductReviews.ProductId == 1 || ProductReviews.ProductId == 4 || ProductReviews.ProductId == 9)
+                                && ProductReviews.Rating > 3
+                                select ProductReviews;
+            foreach (var list in SelectRecords)
+            {
+                Console.WriteLine("ProductID:-" + list.ProductId + " " + "UserID:-" + list.UserId + " " + "Rating:-" + " " + list.Rating + " " + "Review:-" + list.Review + " " + "islike:-" + list.islike);
+            }
+        }
     }
 }
